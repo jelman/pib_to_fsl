fd62c3000694148e267c964903c1b884a9a02bab
Jeremy Elman <jelman@lbl.gov>
2013-12-17 11:20:16 -0800

    add call to concatenate dvr2highres and highres2std coregs

--- voxelwise_pib_userscript.py
+++ voxelwise_pib_userscript.py
@@ -25,21 +25,27 @@ if __name__ == '__main__':
     featdir = os.path.join(basedir,'functional/%(subj)s/run02/Detail.feat')
     
     ### Inputs ###
-    # Specify filenames and filename patterns from pib processing
+    ## Specify filenames and filename patterns from pib processing
     dvr_pattern = os.path.join(pibdir,'rawdvr.nii') # filename pattern of pib image
     meandvr_pattern = os.path.join(pibdir,'mean20min.nii.gz') # filename pattern of mean 20 min pib image
-    # Specify filenames of files used in FSL processing stream
+    ## Specify filenames of files used in FSL processing stream
     highres_pattern = os.path.join(anatdir,'T1_brain.nii.gz') # structural image from FSL processing
-    highres2std_pattern = os.path.join(featdir,
-                                        'reg', 'highres2standard_warp.nii.gz')
     refbrain = os.path.join('/home/jagust/jelman/templates/MNI/data/standard',
-                            'MNI152_T1_2mm_brain.nii.gz') # Standard brain used
-    stdspaceres = None #Specify standard space smoothing resolution. Otherwise, set to None in order to
-                        #calculate from image.
+                        'MNI152_T1_2mm_brain.nii.gz') # Standard brain used
+    #Specify standard space smoothing resolution. Otherwise, set to None in order to
+    #calculate from image.
+    stdspaceres = None 
+    # Determinr whether to use linear or non-linear registration
+    # If linear, set highres2std_pattern as mat file. If non-linear, use warp image
+    nonlinear_reg = False
+    highres2std_pattern = os.path.join(featdir,
+                                        'reg', 'highres2standard.mat')
+
                     
     ### Outputs ###                            
     dvr2highres_mat = 'dvr2highres.mat' # name for pet-> highres coreg mat
     dvr2highres_fname = 'mean20min2highres.nii.gz'
+    dvr2std_mat = 'dvr2std.mat' # name for pet > std coreg mat (not used for non-linear coreg)
     dvr2std_fname = 'dvr2std.nii.gz'    #dvr image in standard space
     pib4d_fname = os.path.join(
                         basedir,        #name of 4d voxelwise covariate file
@@ -67,7 +73,9 @@ if __name__ == '__main__':
         subjdvr = dvr_pattern%{"subj":subj}
         subjmeandvr = meandvr_pattern%{"subj":subj}
         subjhighres = highres_pattern%{"subj":subj}
-        highres2std_warp = highres2std_pattern%{"subj":subj}
+        subjhighres2std_warp = highres2std_pattern%{"subj":subj}
+        subjdvr2std_mat = os.path.join(subjpibdir, dvr2std_mat)
+        stdpib = os.path.join(subjpibdir, dvr2std_fname)
         
         # Estimate and apply smoothing to PIB images
         smoothing = pibprep.est_smoothing(subjfeatdir, subjdvr, stdspaceres)
@@ -85,12 +93,18 @@ if __name__ == '__main__':
         # Warp pib->std. Uses existing structural->std warp and pib->structural premat
         print 'Warping image to standard space...'
 
-        stdpib = os.path.join(subjpibdir, dvr2std_fname)
-        warp_pib2std = pibprep.applywarp(smoothedpib, 
-                                            refbrain, 
-                                            highres2std_warp, 
-                                            dvr2highres_outmat, 
-                                            stdpib)
+
+        if nonlinear_reg:
+            warp_pib2std = pibprep.applywarp(smoothedpib, 
+                                                refbrain, 
+                                                highres2std_warp, 
+                                                dvr2highres_outmat, 
+                                                stdpib)
+        else:
+            dvr2std_outmat = pibprep.concat_xfm(dvr2highres_outmat,
+                                        subjhighres2std_warp,
+                                        subjdvr2std_mat)
+            warp_pib2std = 
         
         # update dict with subj and file
         warpedpib.update({subj:warp_pib2std.out_file}) 
